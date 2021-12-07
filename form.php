1a2f4e0fd038eba23f896f585e20c4811384fd26
INVAMER\GaleanoJ <juan.galeano@invamer.com.co>
2021-12-07 11:03:06 -0500

    Primer commit

---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+	if (isset($_POST['nombre']) && isset($_POST['apellido'])) {
+		$nombre = $_POST['nombre'];
+		$apellido = $_POST['apellido'];
+		echo "<h1>Hola $nombre $apellido</h1>";
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h2>Llene los datos correspondientes</h2>
+	<form method="post" action="form.php">
+		<input type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre"><br><br>
+		<input type="text" name="apellido" id="apellido" placeholder="Ingrese su apellido"><br><br>
+		<input type="submit" value="Send">
+	</form>
+</body>
+</html>
